e5550d8df9ee3f747fe55322895c4eb4d8cc8359
pham phat <phamgiaphat250700@gmail.com>
2022-08-25 23:20:19 +0700

    finish

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import BTChonKinh from "./components/BTChonKinh/BTChonKinh";
+
+function App() {
+  return (
+    <div className="App">
+      <BTChonKinh />
+    </div>
+  );
+}
+
+export default App;
